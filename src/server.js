be64fad1cbd42deae7f405ad0b3fb9968bc65e26
GustavoAlmeida-cy <gustavo.almeida.cy@gmail.com>
2025-07-07 20:22:40 -0300

    fix

--- src/server.js
+++ src/server.js
@@ -2,12 +2,15 @@ import express from "express";
 import path from "path";
 import { fileURLToPath } from "url";
 
+// Get the file path and directory name
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
 const PORT = process.env.PORT || 8383;
 
+// Middlewares
+app.use(express.json());
 app.use(express.static(path.join(__dirname, "..", "public")));
 
 app.get("/", (req, res) => {
