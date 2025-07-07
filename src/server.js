10e74be0c0cd8911b989250f99840be26d1a1c19
GustavoAlmeida-cy <gustavo.almeida.cy@gmail.com>
2025-07-07 20:04:41 -0300

    login front

--- src/server.js
+++ src/server.js
@@ -2,21 +2,16 @@ import express from "express";
 import path from "path";
 import { fileURLToPath } from "url";
 
-// Define __filename and __dirname for ES module scope
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8383;
 
-// Serve static files from the 'public' directory
-app.use(express.static(path.join(__dirname, "../public")));
-
-// Serve static files from the 'src/app' directory
-app.use(express.static(path.join(__dirname, "app")));
+app.use(express.static(path.join(__dirname, "..", "public")));
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "app", "index.html"));
+  res.sendFile(path.join(__dirname, "..", "public", "app", "index.html"));
 });
 
 app.listen(PORT, () => {
