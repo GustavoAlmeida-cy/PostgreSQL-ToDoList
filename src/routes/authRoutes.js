5a821550c75be79c41d38a40c267d57d07c7ed86
GustavoAlmeida-cy <gustavo.almeida.cy@gmail.com>
2025-07-10 22:06:03 -0300

    prisma CRUD

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -2,36 +2,37 @@ import express from "express";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 import db from "../db.js";
+import prisma from "../prismaClient.js";
 
 const router = express.Router();
 
 // Register a new user endpoint /auth/register
-router.post("/register", (req, res) => {
+router.post("/register", async (req, res) => {
   const { username, password } = req.body;
   const hashedPassword = bcrypt.hashSync(password, 8);
 
   // Save the new user and hashed password to the DB
   try {
-    const inserUser = db.prepare(`
-        INSERT INTO users (username, password) 
-        VALUES   (?, ?)  
-    `);
-    const result = inserUser.run(username, hashedPassword);
+    const user = await prisma.user.create({
+      data: {
+        username,
+        password: hashedPassword,
+      },
+    });
 
     // Add a default todo
     const defaultTodo = "Hello, It's your first todo!";
-    const insertTodo = db.prepare(`
-        INSERT INTO todos (user_id, task) 
-        VALUES (?, ?)   
-    `);
-    insertTodo.run(result.lastInsertRowid, defaultTodo);
+    await prisma.todo.create({
+      data: {
+        task: defaultTodo,
+        userId: user.id,
+      },
+    });
 
     // Create a token
-    const token = jwt.sign(
-      { id: result.lastInsertRowid },
-      process.env.JWT_SECRET,
-      { expiresIn: "24h" }
-    );
+    const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, {
+      expiresIn: "24h",
+    });
 
     res.json({ token });
   } catch (error) {
@@ -40,12 +41,15 @@ router.post("/register", (req, res) => {
   }
 });
 
-router.post("/login", (req, res) => {
+router.post("/login", async (req, res) => {
   const { username, password } = req.body;
 
   try {
-    const getUser = db.prepare("SELECT * FROM users WHERE username = ?");
-    const user = getUser.get(username);
+    const user = await prisma.user.findUnique({
+      where: {
+        username: username,
+      },
+    });
 
     // Verifications
     if (!user) {
